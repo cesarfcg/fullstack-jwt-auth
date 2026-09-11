c6b42e78deae87c5109ee7177285b1b3daea698e
cesar <fernandocesar4141@gmail.com>
2026-09-11 12:52:16 -0300

    dividindo as paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,16 @@
-import { useState } from "react";
-import { registerUser } from "./api/client.js";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
 function App() {
-  const [username, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [role, setRole] = useState("USER");
-  const [password, setPassword] = useState("");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const user = {
-      username,
-      email,
-      role,
-      password,
-    };
-
-    try {
-      const data = await registerUser(user);
-
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
   return (
-    <div className="container mt-5">
-      <h1>Criar conta</h1>
-
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label className="form-label">Nome</label>
-
-          <input
-            type="text"
-            className="form-control"
-            value={username}
-            onChange={(event) => setName(event.target.value)}
-          />
-        </div>
-
-        <div className="mb-3">
-          <label className="form-label">Email</label>
-
-          <input
-            type="email"
-            className="form-control"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Tipo de usuário</label>
-
-          <select
-            className="form-select"
-            value={role}
-            onChange={(event) => setRole(event.target.value)}
-          >
-            <option value="USER">Usuário</option>
-            <option value="ADMIN">Administrador</option>
-          </select>
-        </div>
-
-        <div className="mb-3">
-          <label className="form-label">Senha</label>
-
-          <input
-            type="password"
-            className="form-control"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-
-        <button type="submit" className="btn btn-primary">
-          Criar conta
-        </button>
-      </form>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
